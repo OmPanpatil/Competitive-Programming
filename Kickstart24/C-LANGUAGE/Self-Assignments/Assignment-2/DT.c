47afb53f6155346d39d32913fb4f27582b2c7142
Om Panpatil <ompanpatil513@gmail.com>
2024-09-25 20:58:38 +0530

    Day 12 of learning CP did self Assignment and solve some questions

---
+++ Kickstart24/C-LANGUAGE/Self-Assignments/Assignment-2/DT.c
@@ -0,0 +1,10 @@
+
+#include<stdio.h>
+int main(){
+    int DD,MM,YY;
+    printf("Enter the date (DD/MM/YY) : ");
+    scanf("%d/%d/%d",&DD,&MM,&YY);
+    printf("%d/%d/%d",DD,MM,YY);
+    printf("\n");
+    return 0;
+}
