651dd63aa96660742ca693170eff1e6d7c213b07
Om Panpatil <ompanpatil513@gmail.com>
2024-09-26 12:04:28 +0530

    1 Self Assignment question is updated

--- Kickstart24/C-LANGUAGE/Self-Assignments/Assignment-2/DT.c
+++ Kickstart24/C-LANGUAGE/Self-Assignments/Assignment-2/DT.c
@@ -1,3 +1,6 @@
+// Wap to take date as a input in below given format and convert the date format and display the result as given below:
+// User Input date format - "DD/MM/YY" (27/11/2024)
+// Output format - "DAY - DD, Month - MM, Year - YY" (Day-27,Month-11,Year-2024)
 
 #include<stdio.h>
 int main(){
---
+++ Kickstart24/C-LANGUAGE/Self-Assignments/Assignment-2/Time.c
@@ -0,0 +1,13 @@
+// Wap to take input as time in below given format and convert the time format and display the result as given below:
+// User input date format - "HH:MM"
+// Output format - "HH Hour and MM Minute"
+
+#include<stdio.h>
+int main(){
+    int h,m;
+    printf("Enter time (HH : MM) : ");
+    scanf("%d%d",&h,&m);
+    printf("%d Hour and %d Minute",h,m);
+    printf("\n");
+    return 0;
+}
