959dbdce39a744c952af36003fbedf5b9ba26b78
Om Panpatil <ompanpatil513@gmail.com>
2024-09-29 04:10:06 +0530

    Solving self assignments

---
+++ Kickstart24/C-LANGUAGE/Self-Assignments/Assignment-3/5of3Qs.c
@@ -0,0 +1,10 @@
+// Write a program to input a three digit number and display the sum of the digits
+
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter your three digit number : ");
+    scanf("%d",&num);
+    int lastdigit = num%10;
+    
+}
