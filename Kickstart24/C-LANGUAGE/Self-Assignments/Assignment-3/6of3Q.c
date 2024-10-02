4ee766c1e90e30e7a21525009541bbce1bc926ed
Om Panpatil <ompanpatil513@gmail.com>
2024-10-02 16:40:36 +0530

    Day 19 of learning Cp did some basice problem

---
+++ Kickstart24/C-LANGUAGE/Self-Assignments/Assignment-3/6of3Q.c
@@ -0,0 +1,21 @@
+// Write a program which takes a character as an input and displays its ASCII code.
+
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter the charcter u want to know about the ASCII code : ");
+    scanf("%c",&ch);
+    printf("The character as an input with it's ASCII code is %d",ch);
+    // printf("\n");
+    return 0;
+}
+
+// #include <stdio.h>
+// int main()
+// {
+//     char ch; 
+//     printf("Enter a character: "); 
+//     scanf("%c",&ch); 
+//     printf("Ascii code is: %d",ch); 
+//     return 0;
+// }
---
+++ Kickstart24/C-LANGUAGE/Self-Assignments/Assignment-3/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter the charcter u want to know about the ASCII code : ");
+    scanf("%c",&ch);
+    printf("The character %c as an input with it's ASCII code is %d ",ch);
+    printf("\n");
+    return 0;
+}
